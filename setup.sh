4955364ac79bed3f8b682d26f167f9fe12f0d994
Max Braun <maxbraun97@web.de>
2023-09-30 20:27:09 +0200

    Add tmux directory with tpm

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ ln -sf $dotfiles_dir/nvim/lua/custom ~/.config/nvim/lua/custom
 ln -sf $dotfiles_dir/awesome ~/.config/awesome
 ln -sf $dotfiles_dir/.zshrc ~/.zshrc
 ln -sf $dotfiles_dir/.p10k.zsh ~/.p10k.zsh
+ln -sf $dotfiles_dir/.tmux ~/.tmux
