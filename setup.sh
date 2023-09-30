bb085600e7bce90f6a65eb34941ca8058371d937
Max Braun <maxbraun97@web.de>
2023-09-30 20:21:48 +0200

    Add terminal config to setup

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ dotfiles_dir=~/.dotfiles
 
 ### Create symlinks
 ln -sf $dotfiles_dir/nvim/lua/custom ~/.config/nvim/lua/custom
-ln -sf $dorfiles_dir/awesome ~/.config/awesome
+ln -sf $dotfiles_dir/awesome ~/.config/awesome
+ln -sf $dotfiles_dir/.zshrc ~/.zshrc
+ln -sf $dotfiles_dir/.p10k.zsh ~/.p10k.zsh
