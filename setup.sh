27b73ee4d8b95920a3cad4c09d213843c914c6a3
Max Braun <maxbraun97@web.de>
2023-10-01 13:44:40 +0200

    Add various install commands

--- setup.sh
+++ setup.sh
@@ -1,7 +1,27 @@
-#!/bin/env zsh
+#!/bin/env bash
 
 ### Varibales
 dotfiles_dir=~/.dotfiles
+
+### Install packages and programs
+
+# Neovim repo
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+
+sudo apt update
+
+sudo apt install zsh neovim tmux ripgrep -y
+
+# NvChad
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+
+# Lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm -rf lazygit.tar.gz
+rm -rf lazygit
 
 ### Create symlinks
 ln -sf $dotfiles_dir/nvim/lua/custom ~/.config/nvim/lua/custom
@@ -9,4 +29,14 @@ ln -sf $dotfiles_dir/awesome ~/.config/awesome
 ln -sf $dotfiles_dir/.zshrc ~/.zshrc
 ln -sf $dotfiles_dir/.p10k.zsh ~/.p10k.zsh
 ln -sf $dotfiles_dir/.tmux ~/.tmux
-ln -sf $dorfiels_dir/tmux ~/.config/tmux
+ln -sf $dotfiels_dir/tmux ~/.config/tmux
+
+# ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
