db02729f1907f80f2e9c966051b97f9812ab6110
Max Braun <maxbraun97@web.de>
2023-10-08 14:34:23 +0200

    save on exit insert

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -2,11 +2,14 @@ local M = {}
 
 M.general = {
     n = {
-       ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-       ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-       ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
-       ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+        ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+        ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+        ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+        ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
     },
+    i = {
+        ["<ESC>"] = { "<ESC><cmd>w<CR>", "Exit insert mode and save"},
+    }
 }
 
 M.harpoon = {
